5343f7c87e596aab57dd42f795c340d1981bff17
Sharon <sharon.pramesty@ti.ukdw.ac.id>
2019-11-10 21:20:10 +0700

    BANYAK UI SEPER4 JD
    mangat gaes :))

---
+++ HealthyCare/app/src/main/java/com/applicationproject/HealthyCare/BookDokActivity.kt
@@ -0,0 +1,12 @@
+package com.applicationproject.HealthyCare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookDokActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_dok)
+    }
+}
