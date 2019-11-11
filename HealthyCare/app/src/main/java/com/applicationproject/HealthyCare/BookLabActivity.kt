3ddef49766aa1908121d5a21cefbd0cbee22ea1b
Sharon <sharon.pramesty@ti.ukdw.ac.id>
2019-11-11 14:06:18 +0700

    nambah UI UAA TINGBALATINGTUNG
    ya gitu baru dikit, bosen maap

---
+++ HealthyCare/app/src/main/java/com/applicationproject/HealthyCare/BookLabActivity.kt
@@ -0,0 +1,12 @@
+package com.applicationproject.HealthyCare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookLabActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_lab)
+    }
+}
